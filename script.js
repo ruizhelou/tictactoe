b578528d33d7fc8ae4f7835302535f84a5db0dce
Richard <53197263+ruizhelou@users.noreply.github.com>
2025-11-30 16:34:14 +1300

    Implement js logic for playing the game

---
+++ script.js
@@ -0,0 +1,139 @@
+const Board = (function() {
+    const board = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null],
+    ]
+
+    function reset() {
+        for(let row = 0; row < board.length; row++) {
+            for(let col = 0; col < board[row].length; col++) {
+                board[row][col] = null
+            }
+        }
+    }
+
+    function isOutOfBounds(row, col) {
+        return row < 0 || row >= board.length || col < 0 || col >= board[row].length
+    }
+
+    function placeMarker(row, col, marker) {
+        if(isOutOfBounds(row, col) || board[row][col] !== null) {
+            return false
+        }
+        board[row][col] = marker
+        return true
+    }
+
+    function isRowEqual(row) {
+        return board[row].every(col => col === board[row][0] && col !== null);
+    }
+
+    function isColEqual(col) {
+        return board.every(row => row[col] === board[0][col] && row[col] !== null)
+    }
+
+    function isMainDiagonalEqual() {
+        for(let i = 0; i < board.length; i++) {
+            if(board[i][i] === null || board[i][i] !== board[0][0]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function isAntiDiagonalEqual() {
+        for(let i = board.length - 1; i >= 0; i--) {
+            if(board[board.length - i - 1][i] === null || board[board.length - i - 1][i] !== board[0][board.length - 1]) {
+                return false;
+            }
+        }
+        return true
+    }
+
+    function hasWinner() {
+        for(let row = 0; row < board.length; row++) {
+            if(isRowEqual(row)) return true
+        }
+        for(let col = 0; col < board[0].length; col++) {
+            if(isColEqual(col)) return true
+        }
+        if(isMainDiagonalEqual()) return true
+        if(isAntiDiagonalEqual()) return true
+
+        return null
+    }
+
+    function isFull() {
+        for(let row = 0; row < board.length; row++) {
+            for(let col = 0; col < board[row].length; col++) {
+                if(board[row][col] === null) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    return {
+        reset,
+        placeMarker,
+        hasWinner,
+        isFull,
+        board
+    }
+})()
+
+function createPlayer(name, marker) {
+    let score = 0
+
+    function incrementScore() {
+        score++;
+    }
+
+    function getScore() {
+        return score
+    }
+
+    function getMarker() {
+        return marker
+    }
+
+    return {
+        name,
+        getMarker,
+        incrementScore,
+        getScore
+    }
+}
+
+const player1 = createPlayer('Player 1', 'X')
+const player2 = createPlayer('Player 2', 'O')
+
+const Game = (function(board, player1, player2) {
+
+    let currentPlayer = player1
+    
+    function playTurn(row, col) {
+        const validMove = board.placeMarker(row, col, currentPlayer.getMarker())
+        console.log(board.board)
+        if(validMove) {
+            if(board.hasWinner()) {
+                currentPlayer.incrementScore()
+                board.reset()
+                console.log(currentPlayer.name + " won. Score: " + currentPlayer.getScore())
+            } else if(board.isFull()) {
+                board.reset()
+                console.log('tie')
+            } else {
+                currentPlayer = currentPlayer === player1 ? player2 : player1
+            }
+        } else {
+            console.log("Invalid move")
+        }
+    }
+
+    return {
+        playTurn
+    }
+})(Board, player1, player2)
